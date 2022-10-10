cf5a331f8d45e4c199771674ca36163927252634
EyeMLeo <Leonid.Akuta@gmail.com>
2022-10-10 10:31:01 +0200

    javascript uzduotis 1

---
+++ uzd1.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// susikurti kintamuosius vardas, salis, megstamiausiasSkaicius, megstamiausia savaites diena (skaiciais 1-7)
+
+// isvedam i konsole visus savo kintamiuosiu atskirai
+
+// atspausdinam konsoleje savo megstamiausio skaiciau ir dienos daugyba
+
+// atspausdinam konsoleje savo megstamiausio skaiciau is dienos sudeti
+
+
+
+
+let vardas = 'Leonid'
+console.log(vardas);
+
+let salis = 'Lietuva'
+console.log(salis);
+
+let skaicius = 7;
+console.log(skaicius);
+
+let savaitesDiena = 6;
+console.log(savaitesDiena);
+
+console.log(skaicius*savaitesDiena);
+console.log(skaicius+savaitesDiena);
+// atspausdinam konsoleje naudodami kintamuosius "as esu John. noreciau keliausi i Jamaika. Mano megstamiausias skaicius yra 777"
+let sakinys = `As esu ${vardas}. Noreciau keliauti i ${salis}. Mano megstamiausias skaicius ${skaicius}.`;
+console.log(sakinys);
